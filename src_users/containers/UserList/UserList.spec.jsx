0ed61086c450905045fb3b9df20a70e99ae4670e
Sami Karjalainen <sami.karjalainen@geniem.com>
2017-04-28 15:23:40 +0300

    Add more tests

---
+++ src_users/containers/UserList/UserList.spec.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { shallow } from "enzyme";
+import assert from "assert";
+import { UserList } from "./UserList";
+
+// unit tests for the UserListElement component
+describe.skip('UserList component', () => {
+  describe('render()', () => {
+    it('should render the progressbar', () => {
+      const props = {users: []};
+      const wrapper = shallow(<UserList {...props} />);
+      assert.equal(wrapper.length, 1);
+    });
+  });
+});
