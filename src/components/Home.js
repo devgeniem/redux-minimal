08ae9b0c60ff7fc183dad71d390c67d422a3e75a
MPuhakka <mika.puhakka@geniem.com>
2017-06-02 14:14:00 +0300

    babel change

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,13 @@
 import React from "react";
 
-import {Header, Footer, Layout } from 'antd';
+import { DatePicker } from 'antd';
 
 export default class Home extends React.Component {
   render() {
     return (
-      <Layout className="page-home">
-        <Footer>
-          penis
-        </Footer>
-        <h4>Hello world!</h4>
-      </Layout>
+      <div>
+        
+      </div>
     );
   }
 }
