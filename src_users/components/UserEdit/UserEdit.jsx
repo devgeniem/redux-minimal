dcf119c434b782351c106844cf100a52d506b59e
MPuhakka <mika.puhakka@geniem.com>
2017-06-02 13:26:35 +0300

    some UI stuff

--- src_users/components/UserEdit/UserEdit.jsx
+++ src_users/components/UserEdit/UserEdit.jsx
@@ -2,39 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
-import { Col, PageHeader, Row } from 'react-bootstrap';
+import { Col, PageHeader, Row, FormGroup, FormControl, ControlLabel, HelpBlock, Button } from 'react-bootstrap';
 // import { FormField, FormSubmit } from '../../components/';
-import UserActions from '../../actions/users';
+
 
 export class UserEdit extends React.Component {
   constructor(props) {
     super(props);
-
-    this.formSubmit = this.formSubmit.bind(this);
-  }
-
-  formSubmit(values) {
-    const { dispatch } = this.props;
-
-    const user = { id: values.id || 0, username: values.username, job: values.job };
-
-    return new Promise((resolve, reject) => {
-      dispatch({
-        type: 'USERS_ADD_EDIT',
-        user: {
-          id: values.id || 0,
-          username: values.username,
-          job: values.job,
-        },
-        callbackError: (error) => {
-          reject(new SubmissionError({ _error: error }));
-        },
-        callbackSuccess: () => {
-          dispatch(push('/'));
-          resolve();
-        },
-      });
-    });
   }
 
   render() {
@@ -42,14 +16,25 @@ export class UserEdit extends React.Component {
     return (
       <div className="page-user-edit">
         <PageHeader>{`User ${user.id ? 'edit' : 'add'}`}</PageHeader>
-        <Col xs={12}>
-          <Row>
-            <input placeholder="username" type="text" />
-          </Row>
-          <Row>
-            <input placeholder="job" type="text" />
-          </Row>
-        </Col>
+        <form>
+            <FormGroup
+              controlId="formBasicText">
+              <ControlLabel>User info</ControlLabel>
+              <FormControl
+                type="text"
+                placeholder="user"
+              />
+              <FormControl
+                type="text"
+                placeholder="job"
+              />
+              <FormControl.Feedback />
+
+              <Button bsStyle="primary">
+                penis
+              </Button>
+            </FormGroup>
+          </form>
       </div>
     );
   }
@@ -58,7 +43,6 @@ export class UserEdit extends React.Component {
 UserEdit.PropTypes = {
   dispatch: PropTypes.func.isRequired,
   user: PropTypes.shape({}).isRequired,
-  handleSubmit: PropTypes.func.isRequired,
   error: PropTypes.string,
   invalid: PropTypes.bool,
   submitting: PropTypes.bool,
--- src_users/components/index.js
+++ src_users/components/index.js
@@ -1,5 +1,3 @@
-export FormField from './Form/FormField';
-export FormSubmit from './Form/FormSubmit';
 export Home from './Home/Home';
 export Menu from './Menu/Menu';
 export NotFound from './NotFound/NotFound';
